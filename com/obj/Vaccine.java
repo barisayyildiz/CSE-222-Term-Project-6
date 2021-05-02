4b317c446198295330220816d7603a21d2c46f0f
barisayyildiz <barisayyildiz.1799@gmail.com>
2021-05-02 19:06:31 +0300

    Vaccine(aşı) classı yazıldı

--- com/obj/Vaccine.java
+++ com/obj/Vaccine.java
@@ -4,12 +4,25 @@ public class Vaccine {
 	private int number;
 	private VaccineType type;
 
-	public Vaccine(){
-		// ...
+	public Vaccine(int number, VaccineType type){
+		this.number = number;
+		this.type = type;
+	}
+
+	public void setNumber(int number){
+		this.number = number;
+	}
+
+	public void setType(VaccineType type){
+		this.type = type;
+	}
+
+	public int getNumber(){
+		return this.number;
+	}
+
+	public VaccineType getType(){
+		return this.type;
 	}
 	
-
-	// getter setter eklenecek
-
-
 }

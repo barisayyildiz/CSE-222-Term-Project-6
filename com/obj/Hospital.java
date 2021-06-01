372aa414fefc4f9b733455383186fbbea7736ccf
barisayyildiz <barisayyildiz.1799@gmail.com>
2021-06-01 21:48:16 +0300

    Hospital class ı yazıldı

--- com/obj/Hospital.java
+++ com/obj/Hospital.java
@@ -1,28 +1,36 @@
 package com.obj;
 
 import com.users.*;
+import java.util.ArrayList;
 
 public class Hospital {
 
 	// hashcode
 	private int id;
-
 	private Ministry ministry;
-
 	private HeadPhysician headPhysician;
 
 	private int numOfBeds;
 	private int numOfTests;
 	// aşı tipleri ve sayılarını içeren 2d bir array yazılacak
 
+	private ArrayList<Vaccine> vaccines;
+
 
 	public Hospital(HeadPhysician headPhysician, Ministry ministry){
-		// constructor yazılacak
 		this.id = this.hashCode();
 		this.ministry = ministry;
 		this.numOfBeds = 100;
 		this.numOfTests = 1000;
 		this.headPhysician = headPhysician;
+		
+		// aşılar eklendi
+		this.vaccines = new ArrayList<Vaccine>();
+		this.vaccines.add(new Vaccine(100, VaccineType.ASTRAZENECA));
+		this.vaccines.add(new Vaccine(100, VaccineType.BIOENTECH));
+		this.vaccines.add(new Vaccine(100, VaccineType.KAYSERI));
+		this.vaccines.add(new Vaccine(100, VaccineType.SINOVAC));
+		this.vaccines.add(new Vaccine(100, VaccineType.SPUTNIK));
 
 	}
 

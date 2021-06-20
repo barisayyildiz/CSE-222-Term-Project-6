c7ac3f38f9ca66c54677aa51407d7bdb0deacf96
Öner Ercan <onerercan95@hotmail.com>
2021-06-20 13:45:49 +0300

    Ministry v1

--- com/obj/Ministry.java
+++ com/obj/Ministry.java
@@ -1,32 +1,30 @@
 package com.obj;
 
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-import java.util.TreeSet;
+import java.util.*;
 
 // File Handling
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 import com.users.*;
 
 public class Ministry {
+	// Data Fields
 
 	private Minister minister;
-	
-	private ArrayList<Hospital> hospitals;
+
+	private HashMap<Integer, Hospital> hospitals;
 	private ArrayList<HealthEmployee> healthEmployees;
 	private ArrayList<Vaccine> vaccines;
 	private PriorityQueue<Integer> vaccinationOrder;
 	private TreeSet<Patient> patients;
-	
+
+	// Constructors
 
 	public Ministry(Minister minister){
-		// constructor yazılacak, txt okunacak...
 		this.minister = minister;
 
-		this.hospitals = new ArrayList<Hospital>();
+		this.hospitals = new HashMap<Integer, Hospital>();
 		this.healthEmployees = new ArrayList<HealthEmployee>();
 		this.vaccines = new ArrayList<Vaccine>();
 		this.vaccinationOrder = new PriorityQueue<Integer>();
@@ -40,7 +38,7 @@ public class Ministry {
 			while (scanner.hasNextLine()) {
 				String temp = scanner.nextLine();
 				String arr[] = temp.split(",");
-				
+
 				// sırasıyla
 				// tckno,first_name,last_name,password,age,is_covid,is_sick,is_smoking,is_vaccinated
 				this.patients.add(new Patient(arr[0], arr[1], arr[2], arr[3], Integer.parseInt(arr[4]), Boolean.valueOf(arr[5]), Boolean.valueOf(arr[6]), Boolean.valueOf(arr[7]), Boolean.valueOf(arr[8])  ));
@@ -58,7 +56,7 @@ public class Ministry {
 			while (scanner.hasNextLine()) {
 				String temp = scanner.nextLine();
 				String arr[] = temp.split(",");
-				
+
 				// sırasıyla
 				// tckno,first_name,last_name,password,age,job
 				switch(arr[5])
@@ -78,15 +76,115 @@ public class Ministry {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+	}
 
+	// Functions
 
+	public void addHospital(){
+		/*
+		int maxAgeIndex = 0;
+
+		for(int i = 0; i < healthEmployees.size(); i++){
+			if(healthEmployees.get(i) instanceof Doctor){
+				if(i == 0){
+					maxAgeIndex = i;
+				}
+				else if(((Doctor) healthEmployees.get(i)).getAge() > ){
+					healthEmployees.remove(i);
+					return;
+				}
+			}
+		}
+
+		Hospital newHospital = new Hospital(,this);
+		hospitals.put(newHospital.getId(), newHospital);
+		*/
+	}
+
+	public void removeHospital(int id){
+		hospitals.remove(id);
+	}
+
+	public void addDoctor(String tckno, String firstName, String lastName, String password, int age){
+		for(int i = 0; i < healthEmployees.size(); i++){
+			if(healthEmployees.get(i).equals(tckno) == true){
+				return;
+			}
+		}
+
+		healthEmployees.add(new Doctor(tckno, firstName, lastName, password, age));
+	}
+	public void removeDoctor(String tckno){
+		for(int i = 0; i < healthEmployees.size(); i++){
+			if(healthEmployees.get(i).equals(tckno) == true){
+				healthEmployees.remove(i);
+				return;
+			}
+		}
+	}
+
+	public void addNurse(String tckno, String firstName, String lastName, String password, int age){
+		for(int i = 0; i < healthEmployees.size(); i++){
+			if(healthEmployees.get(i).equals(tckno) == true){
+				return;
+			}
+		}
+
+		healthEmployees.add(new Nurse(tckno, firstName, lastName, password, age));
+	}
+	public void removeNurse(String tckno){
+		for(int i = 0; i < healthEmployees.size(); i++){
+			if(healthEmployees.get(i).equals(tckno) == true){
+				healthEmployees.remove(i);
+				return;
+			}
+		}
+	}
+
+	public void addVaccine(int vacNumber, VaccineType vacType) {
+		for (int i = 0; i < vaccines.size(); i++) {
+			if (vaccines.get(i).getType() == vacType) {
+				return;
+			}
+		}
+
+		vaccines.add(new Vaccine(vacNumber, vacType));
+	}
+
+	public Patient register(String firstName, String lastName, String tckno, String password, int age, boolean isSmoking, boolean isVaccinated, boolean isCovid, boolean isSick){
+		Set<Patient> set = patients;
+
+		for(Patient patient : patients){
+			if(patient.getTckno().equals(tckno) == true){
+				return null;
+			}
+		}
+
+		Patient newPatient = new Patient(tckno, firstName, lastName, password, age, isSmoking, isVaccinated, isCovid, isSick);
+		patients.add(newPatient);
+
+		return newPatient;
+	}
+
+	public void removePatient(String tckno){
+		Set<Patient> set = patients;
+
+		for(Patient patient : patients){
+			if(patient.getTckno().equals(tckno) == true){
+				patients.remove(patient);
+				return;
+			}
+		}
+	}
+	public void addVaccinationOrder(Integer value){
+		vaccinationOrder.add(value);
 	}
 
 	public Minister getMinister(){
 		return this.minister;
 	}
 
-	public ArrayList<Hospital> getHospitals(){
+	public HashMap<Integer, Hospital> getHospitals(){
 		return this.hospitals;
 	}
 
@@ -110,9 +208,4 @@ public class Ministry {
 	public static void getData(int hospitalId){
 		return;
 	}
-
-	public static Patient register(String firstName, String lastName, String tckno, String password, int age, boolean isSmoking, boolean isVaccinated, boolean isCovid, boolean isSick){
-		return new Patient();
-	}
-
 }
